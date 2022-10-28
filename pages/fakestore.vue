635717457a87a19c2ebe3f0903f234919f9fc508
AungPaingSoe <aungpaingsoe097238@gmail.com>
2022-10-28 14:50:39 +0630

    axios get

---
+++ pages/fakestore.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{ products }}
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        products : []
+      }
+    },
+    created(){
+      this.$axios.$get('https://fakestoreapi.com/products')
+      .then((res)=>{
+        this.products = res;
+      })
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
