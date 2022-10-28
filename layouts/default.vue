a8c92eac9bab97b065610f57a67f8603826b9076
AungPaingSoe <aungpaingsoe097238@gmail.com>
2022-10-28 13:12:37 +0630

    init

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nuxt-link to="/">Index</nuxt-link>
+    <nuxt-link to="/about">About</nuxt-link>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Index IndexPage
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
