3a14dfbc67e18bf412a0c107a2f6d7f33cfaf8a0
AungPaingSoe <aungpaingsoe097238@gmail.com>
2022-10-28 13:28:22 +0630

    custom error layout

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    This is error page
+    {{ error }}
+  </div>
+</template>
+
+<script>
+  export default {
+    layout : 'error-layout',
+    props : {
+      error : {
+        type : String
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
